06c383343f42af2e604f8632b9e7bae6bacfd697
enricobacis <enrico.bacis@gmail.com>
2018-05-23 18:10:18 +0200

    do the magic
    convert the excel file into a sqlite3 database

--- scimagojr.py
+++ scimagojr.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
 
-from six.moves import configparser
+import argparse
+import sqlite3
 import pandas
 
 
-def main():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    filename = config.get('data', 'scimagojr_file')
-    print('[*] reading file: {}'.format(filename))
+def convert(scimagofile, dbfile):
 
-    data = pandas.read_excel(filename)
-    print(data)
+    print('[*] reading file: {}'.format(scimagofile))
+    venue = pandas.read_excel(scimagofile)
+
+    print('[*] normalizing column names')
+    venue.columns = venue.columns.str.replace('(^\W+|\W+$)', '')
+    venue.columns = venue.columns.str.replace('\W+', '_')
+
+    print('[*] normalizing ISSNs (one entry per ISSN)')
+    issns = venue.Issn.str.replace('\s*ISSN\s*', '').str.split(
+        '\s*,\s*', expand=True).stack()
+    indices = issns.index.get_level_values(0)
+    venue = venue.loc[indices].copy()
+    venue["Issn"] = issns.values
+
+    print('[*] creating Venue_Category data')
+    venue_category = venue.Categories.str.split('\s*;\s*', expand=True).stack()
+    venue_category.index = venue_category.index.droplevel(1)
+    venue_category = pandas.DataFrame(venue_category)
+    # https://regex101.com/r/rd280S/1
+    venue_category = venue_category[0].str.extract(
+        r'^(?P<Category>.*?)(?:(?:\s*\()(?P<Quartile>Q\d)(?:\)))?$')
+
+    with sqlite3.connect(dbfile) as connection:
+        print('[*] creating Venue table in: {}'.format(dbfile))
+        venue.to_sql('Venue', connection, index=True, index_label='venue_id')
+
+        print('[*] creating Venue_Category table in: {}'.format(dbfile))
+        venue_category.to_sql('Venue_Category', connection,
+                              index=True, index_label='venue_id')
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description='convert scimagojr excel file to sqlite')
+    parser.add_argument('SCIMAGOFILE', help='scimago excel input file')
+    parser.add_argument('DBFILE', help='sqlite3 output file')
+    args = parser.parse_args()
+    convert(args.SCIMAGOFILE, args.DBFILE)
